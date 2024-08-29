1520cb4d30c445f9858adf8cd59576827969c642
Pedro Galembeck <galembeckpedro@gmail.com>
2024-08-29 13:45:11 -0300

    feat: add consts realted to clubs-card & testimonials

---
+++ src/lib/data.ts
@@ -0,0 +1,56 @@
+export const clubsCardsData = [
+  {
+    title: "Chess",
+    description: "We are a vibrant community of chess enthusiasts dedicated to promoting the game and fostering a supportive environment for players of all levels.",
+    items: [
+      "Regular Training Sessions",
+      "Competitive Play", 
+      "Social Events", 
+      "Educational Resources",
+      "Community and Support",
+    ],
+  },
+  {
+    title: "Astronomy",
+    description: "Whether you’re looking to expand your knowledge of the cosmos or simply enjoy the beauty of the night sky with like-minded individuals, the Astronomy Enthusiasts Club welcomes you. Come be a part of our stellar community and embark on an exciting journey of discovery and wonder.",
+    items: [
+      "Observation Nights",
+      "Educational Workshops",
+      "Exclusive Events",
+      "Community and Networking",
+      "Access to Resources",
+    ],
+  },
+  {
+    title: "Olympics",
+    description: "We are a dynamic community of knowledge enthusiasts dedicated to promoting intellectual growth and fostering a supportive environment for learners of all levels.",
+    items: [
+      "Regular Quiz Competitions",
+      "Educational Workshops",
+      "Exclusive Events",
+      "Community and Networking",
+      "Access to Resources",
+    ],
+  },
+] as const;
+
+export const testimonials = [
+  {
+    icon: "https://avatars.githubusercontent.com/u/51977156?v=4",
+    title: "Pedro Galembeck",
+    content:
+      "“Joining the Chess Olympic Club has been an incredible experience. The training sessions are top-notch, and I’ve learned so much from the experienced coaches. Competing in tournaments has been both challenging and rewarding, and I’ve made great friends along the way. The sense of community here is amazing, and I always look forward to our game nights and events.”",
+  },
+  {
+    icon: "https://avatars.githubusercontent.com/u/33735654?s=100&v=4",
+    title: "Diego Fernandes",
+    content:
+      "“The Astronomy Olympic Club has reignited my passion for stargazing. The observation nights are magical, and the workshops have deepened my understanding of the cosmos. The club’s events, like meteor shower viewings and planetarium visits, are always well-organized and educational. I’ve met so many wonderful people who share my love for astronomy, and it’s been a fantastic journey of discovery.”",
+  },
+  {
+    icon: "https://avatars.githubusercontent.com/u/56325350?s=100&v=4",
+    title: "Murilo Henrique",
+    content:
+      "“Being part of the Olympics Club has been a game-changer for me. The quiz competitions are exciting and cover such a wide range of topics. The workshops and guest lectures have expanded my knowledge and inspired me to keep learning. The community is incredibly supportive, and I’ve made lifelong friends here. This club has truly enriched my intellectual life.”",
+  },
+] as const;
