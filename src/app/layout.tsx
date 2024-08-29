d5bbe037184d3de5f51f9a4e448a7baffe927a5a
Pedro Galembeck <galembeckpedro@gmail.com>
2024-08-28 22:57:08 -0300

    refactor: update layout component with new font families and navigation components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import { Alfa_Slab_One, Bungee_Inline, Poppins } from "next/font/google";
 
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
@@ -8,8 +8,8 @@ import { SITE_CONFIG } from "@/config";
 
 import { cn } from "@/lib/utils";
 
-import { Header } from "@/components/header";
-import { Footer } from "@/components/footer";
+import { Header } from "@/components/navigation/header";
+import { Footer } from "@/components/navigation/footer";
 
 import "./globals.css";
 
@@ -17,6 +17,16 @@ const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   subsets: ["latin"],
   variable: '--font-poppins',
+});
+const bungeeInline = Bungee_Inline({ 
+  weight: ['400'],
+  subsets: ["latin"],
+  variable: '--font-bungee-inline',
+});
+const alfaSlabOne = Alfa_Slab_One({ 
+  weight: ['400'],
+  subsets: ["latin"],
+  variable: '--font-alfa-slab-one',
 });
 
 export const metadata = SITE_CONFIG;
@@ -28,7 +38,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth overflow-x-hidden">
-      <body className={cn(poppins.className, "min-h-screen bg-slate-950 text-white antialiased")}>
+      <body className={cn(
+        poppins.className, 
+        bungeeInline.variable, 
+        alfaSlabOne.variable, 
+        "min-h-screen bg-slate-950 text-white antialiased")}
+      >
         <ClerkProvider appearance={{baseTheme: dark}}>
           <Header />
           {children}
