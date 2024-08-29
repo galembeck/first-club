a1fedf43c96462b32cad4a5498728e00c96d5e88
Pedro Galembeck <galembeckpedro@gmail.com>
2024-08-29 13:42:38 -0300

    refactor: add olympic-clubs and testimonials components

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,15 @@
 import { Intro } from "./components/intro";
 import { Calendar } from "./components/calendar";
+import { OlympicClubs } from "./components/olympic-clubs";
+import { Testimonials } from "./components/testimonials";
 
 export default function HomePage() {
   return(
     <main>
       <Intro />
       <Calendar />
+      <OlympicClubs />
+      <Testimonials />
     </main>
   );
 }
